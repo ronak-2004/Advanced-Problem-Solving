3ba570ad6b942bde7424a36c4cba703323f040e8
ronak-2004 <168120265+ronak-2004@users.noreply.github.com>
2025-07-17 00:10:30 +0530

    Update is_String_divisible_by_n.cpp

--- utilities/is_String_divisible_by_n.cpp
+++ utilities/is_String_divisible_by_n.cpp
@@ -1,2 +1,31 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
+bool isDivisibleByN(string &str,int n){
+    int rem=0;
+    for(char c:str){
+        rem = (10*rem+(c-'0'))%n;
+    }
+    return rem==0;
+}
+
+int main(){
+    string s;
+    int n;
+    //Enter string: 
+    cin>>s;
+    //Enter n (divisor)
+    cin>>n;
+    if(isDivisibleByN(s,n)){
+        cout<<"Is divisible";
+    }
+    else cout<<"Is not Divisible";
+    return 0;
+}
+
+
+/*
+TC : O(s.length)
+Aux. Space: o(1)
+*/
